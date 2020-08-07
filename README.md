109c7c9fb6fbc39509acaa16f3e4fff94b6db2b9
SadeqSp <sadeq.sp@gmail.com>
2020-08-07 14:49:19 +0430

    README.md edited > PATH

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Simple routing system for an php web application
+
+- notes:
+Put your own local path in config.php[L5] and .htaccess[L5]
--- _func/db.php
+++ _func/db.php
@@ -9,6 +9,8 @@ $dbname = "_test";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
+/*
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error()."<br>");} 
 else {echo "Connected successfully<br>";}
+*/
